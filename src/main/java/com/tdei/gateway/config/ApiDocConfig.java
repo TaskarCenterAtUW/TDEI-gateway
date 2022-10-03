1cd8dab3b36d1e76878ec3ef6d1ef7b98b47d882
Naresh Kumar D <nareshd@gaussiansolutions.com>
2022-10-03 11:40:31 +0530

    Application changes made
    #88 is fixed with this.

--- src/main/java/com/tdei/gateway/config/ApiDocConfig.java
+++ src/main/java/com/tdei/gateway/config/ApiDocConfig.java
@@ -5,12 +5,20 @@ import io.swagger.v3.oas.models.OpenAPI;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import com.tdei.gateway.middleware.LogInterceptor;
+
 
 @Configuration
-public class ApiDocConfig {
+public class ApiDocConfig implements WebMvcConfigurer {
 
     @Autowired
     private ApplicationProperties applicationProperties;
+
+    @Autowired
+    LogInterceptor logInterceptor;
 
     @Bean
     public OpenAPI springShopOpenAPI() {
@@ -22,5 +30,10 @@ public class ApiDocConfig {
                                 .email(applicationProperties.getSwagger().getContact().getEmail())
                                 .url(applicationProperties.getSwagger().getContact().getUrl())))
                 ;
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+         registry.addInterceptor(logInterceptor);
     }
 }
