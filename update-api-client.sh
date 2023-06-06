ed35739ece5308374da8fe68597c69bb03e22a34
Prashanth <santaduda@gmail.com>
2023-06-06 13:38:11 +0530

    Send endPoint as input to api client workflow

--- update-api-client.sh
+++ update-api-client.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 result=`git diff --name-only HEAD HEAD~1`
-
+environment=$2
 # Check if atleast one file is modified in src/main folder
 if [[ $result == *"src/main"* ]]; then
     echo "Changes present in src/main folder.."
@@ -11,7 +11,7 @@ if [[ $result == *"src/main"* ]]; then
         -H "Authorization: Bearer $1"\
         -H "X-GitHub-Api-Version: 2022-11-28" \
         https://api.github.com/repos/TaskarCenterAtUW/TDEI-api-client/actions/workflows/autogenerate_api_client/dispatches \
-        -d '{"ref":"main"}'
+        -d '{"ref":"main", "inputs":{"endPoint" : "https://tdei-gateway-'$environment'.azurewebsites.net/v3/api-docs"}}'
 else
     echo "No changes in src/main folder"
 fi
